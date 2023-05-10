0f0863e8b60ec312aa0332d34c15b154754a1666
Omkar Sathe <124599746+Omkars2000@users.noreply.github.com>
2023-05-10 16:06:48 +0530

    Update App.js
    NA

--- User_Data_Handling_React_Folder/userinfo/src/App.js
+++ User_Data_Handling_React_Folder/userinfo/src/App.js
@@ -1,6 +1,6 @@
 
 import React from "react";
-import { Routes, Route } from 'react-router-dom';
+// import { Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddUser from "./AddUser";
 import DownloadPDF from "./downloadPdf";
